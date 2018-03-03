3113b9b3e32931b648024c39486012750a8af244
aravijai777 <30646385+aravijai777@users.noreply.github.com>
2018-03-03 22:31:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,12 +19,16 @@ var articleOne = {
                 This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
             </p>`
 };
-
+function createTemplate (data) {
+    var title= data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
 var htmlTemplate = `
 <html>
     <head>
         <title>
-            Article-one | ARAVIND
+            ${title}
         </title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
            <link href="/ui/style.css" rel="stylesheet" />
@@ -35,32 +39,25 @@ var htmlTemplate = `
         <a href="/">Home</a>
         </div>
         <hr/>
-        <h3>Article one</h3>
+        <h3>${heading}</h3>
         <div>
-            Mar 2 ,2018
+             ${date}
         </div>
         <div>
-            <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
+            ${Content}
             </div>
             </div>
 </body>    
 </html>
-
 `;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req , res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function(req , res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
