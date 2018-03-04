b4b43ed356c9c895121c5a84a4431e00b44a2fde
aravijai777 <30646385+aravijai777@users.noreply.github.com>
2018-03-04 15:56:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var articles = {
             </p>
   ` }
 };
-function createTemplate (data) {
+function createTemplate(data) {
     var title = data.title;
     var heading = data.heading;
     var date = data.date;
