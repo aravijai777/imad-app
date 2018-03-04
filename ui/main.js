d8d4443cd534350c5b43b8d19f100cb13918b4d5
aravijai777 <30646385+aravijai777@users.noreply.github.com>
2018-03-05 00:10:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,7 @@ button.onclick = function () {
   request.send(null);
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
@@ -43,7 +42,8 @@ submit.onclick = function () {
         }
     }
 };    
-
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
   request.open('GET','http://aravindjayakumar.imad.hasura-app.io/submit-name?name=' +name,true);
   request.send(null);
     
