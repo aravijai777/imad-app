6a0f28fca80093024e3661ea1625b23ada12cd51
aravijai777 <30646385+aravijai777@users.noreply.github.com>
2018-03-04 15:39:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+var img = document.getElementById('img');
+img.onClick = function () {
+    img.style.marginLeft = '100px';
+}
